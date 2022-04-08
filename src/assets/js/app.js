072d6118b5f7bca8932cc764d2b50c7e8f21a440
frontdevruso <frontdevruso@gmail.com>
2022-04-08 17:57:37 +0300

    main page & header

--- src/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-const blogItemTitle = document.querySelectorAll('.main-slider__item-info p');
-
-if (blogItemTitle) {
-    blogItemTitle.forEach(function(item) {
-        item.style.setProperty("-webkit-box-orient", "vertical");
-    })
-}
