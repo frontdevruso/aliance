e02ec3d16d8ac3a6c23c2971b057aa4bf2d29577
frontdevruso <frontdevruso@gmail.com>
2022-04-15 11:20:36 +0300

    benefits slider & responsive

---
+++ src/assets/js/benefits-slider.js
@@ -0,0 +1,21 @@
+let benefitsSlider = new Swiper(".about-preview__benefits", {
+    slidesPerView: "auto",
+    grabCursor: true,
+
+    navigation: {
+      nextEl: ".about-preview__benefits--next",
+      prevEl: ".about-preview__benefits--prev",
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+        1210: {
+            slidesPerView: 5,
+        },
+    }
+});
