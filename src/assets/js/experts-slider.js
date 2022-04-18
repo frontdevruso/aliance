3c71a8985504d599d18e5d4125561c3d6e3a083c
backdevruso <feedback0225@gmail.com>
2022-04-18 18:46:56 +0300

    the-title

--- src/assets/js/experts-slider.js
+++ src/assets/js/experts-slider.js
@@ -1,16 +1,18 @@
-let expertsSlider = new Swiper(".experts__slider", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    grabCursor: true,
-
-    navigation: {
-      nextEl: ".experts__slider--next",
-      prevEl: ".experts__slider--prev",
-    },
-
-    breakpoints: {
-      1100: {
-          slidesPerView: 2,
+if(document.querySelector('.experts__slider')) {
+  let expertsSlider = new Swiper(".experts__slider", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      grabCursor: true,
+  
+      navigation: {
+        nextEl: ".experts__slider--next",
+        prevEl: ".experts__slider--prev",
       },
-  }
-});
+  
+      breakpoints: {
+        1100: {
+            slidesPerView: 2,
+        },
+    }
+  });
+}
--- src/assets/js/work-scheme-slider.js
+++ src/assets/js/work-scheme-slider.js
@@ -1,21 +1,23 @@
-let benefitsSlider = new Swiper(".work-scheme__table", {
-    slidesPerView: "auto",
-    grabCursor: true,
-
-    navigation: {
-      nextEl: ".work-scheme__table--next",
-      prevEl: ".work-scheme__table--prev",
-    },
-
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
+if(document.querySelector('.work-scheme__table')) {
+    let benefitsSlider = new Swiper(".work-scheme__table", {
+        slidesPerView: "auto",
+        grabCursor: true,
+    
+        navigation: {
+          nextEl: ".work-scheme__table--next",
+          prevEl: ".work-scheme__table--prev",
         },
-        1024: {
-            slidesPerView: 3,
-        },
-        1210: {
-            slidesPerView: 4,
-        },
-    }
-});
+    
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+            1210: {
+                slidesPerView: 4,
+            },
+        }
+    });
+}
