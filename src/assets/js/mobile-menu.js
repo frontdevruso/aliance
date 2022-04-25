4617f00b1c759147796781f27713a93becb2b953
backdevruso <feedback0225@gmail.com>
2022-04-25 13:34:57 +0300

    mobile-menu

---
+++ src/assets/js/mobile-menu.js
@@ -0,0 +1,4 @@
+if (document.querySelector('.menu')) {
+    const allLinks = document.querySelectorAll('.menu__container ul li');
+    const allDropDownLinks = document.querySelectorAll('.menu__container ul li .menu__drop-down .menu__drop-down-item');
+}
